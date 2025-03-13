4a7b9298ad0c2ceecbd0b4de90404044c1e4cf86
mettil13 <matildemariano01@gmail.com>
2025-03-13 13:54:32 +0100

    Printone

---
+++ Source/monstersSanctuary/Printone.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Printone.h"
+
+// Sets default values
+APrintone::APrintone()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APrintone::BeginPlay()
+{
+	Super::BeginPlay();
+	GetActorLocation();
+	UE_LOG(LogTemp, Warning, TEXT("Inizializzato"));
+	GetActorLocation();
+	
+}
+
+// Called every frame
+void APrintone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
