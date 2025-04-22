93ed296fc63136fa9c7e9aca9f177be9f6c3689a
ElanProject <126772054+ElanSSHProject@users.noreply.github.com>
2025-04-22 17:00:35 +0200

    photo manager evaluating system fix

--- Source/monstersSanctuary/PhotoManager.cpp
+++ Source/monstersSanctuary/PhotoManager.cpp
@@ -5,25 +5,45 @@
 void UPhotoManager::EvaluateScene(
 	////inputs
 	TArray<AActor*> allCreatures,
-	//TArray<AActor*> allInteractable,
+	TArray<AActor*> allInteractable,
 	//TArray<AActor*> allFornitures,
 	// outputs
 	FEvaluateSceneResult& result
 ) 
 {
-	FCreatureStats stats = FCreatureStats();
+	
 	result = FEvaluateSceneResult();
-	result.value = 0;
+	result.value = 0; // can be only positive and defines the intensity of patreon donations
+	result.brokenStructureVisible = false;
+	result.lowLevelStructureVisible = false;
+	result.sadCreatureVisible = false;
+	result.CreatureIsntDoingAnything = false;
+	result.needFoodCreatureVisible = false;
+	result.needWaterCreatureVisible = false;
+	result.needBedCreatureVisible = false;
+	result.needCleanCreatureVisible = false;
 
+	// evaluate creatures inside
 	float statsMedian = 0;
-
+	int c = 0;
 	for (AActor* creature : allCreatures) {
-		//UE_LOG(LogTemp, Warning, TEXT("%s"), *creature->GetName());
+		if (!creature->WasRecentlyRendered(0.5)) {
+			continue; // skip the creature if it is not rendered
+		}
+		FCreatureStats stats;
 		IAI_CreatureCharacterInterface* creatureInterface = Cast<IAI_CreatureCharacterInterface>(creature);
 		creatureInterface->Execute_GetCreatureStats(creature, stats);
 
 		float currentMedian = 0;
 
+		// hint construction
+		if (stats.hunger < statsMinimumToConsiderPositive.hunger) result.needFoodCreatureVisible = true;
+		if (stats.thirst < statsMinimumToConsiderPositive.thirst) result.needWaterCreatureVisible = true;
+		if (stats.slumber < statsMinimumToConsiderPositive.slumber) result.needBedCreatureVisible = true;
+		if (stats.cleanness < statsMinimumToConsiderPositive.cleanness) result.needCleanCreatureVisible = true;
+		if (stats.happyness < statsMinimumToConsiderPositive.happyness) result.sadCreatureVisible = true;
+
+		// median cunstruction
 		currentMedian += (stats.hunger * statsEvaluateMultiplier.hunger) + statsEvaluateAdder.hunger;
 		currentMedian += (stats.thirst * statsEvaluateMultiplier.thirst) + statsEvaluateAdder.thirst;
 		currentMedian += (stats.slumber * statsEvaluateMultiplier.slumber) + statsEvaluateAdder.slumber;
@@ -32,15 +52,44 @@ void UPhotoManager::EvaluateScene(
 		currentMedian /= 5.0;
 		UE_LOG(LogTemp, Warning, TEXT("creature %s evaluated with : %f"), *creature->GetName(), currentMedian);
 		statsMedian += currentMedian;
+		c++;
+	}
+	if (c != 0) statsMedian /= c;
+	statsMedian *= statsMedianMultiplier;
+	UE_LOG(LogTemp, Warning, TEXT("stats median for creatures : %f"), statsMedian);
+
+	// evaluate interactable inside
+	float interactableMedian = 0;
+	c = 0;
+	for (AActor* interactable : allInteractable) {
+		if (interactable == NULL || !interactable->WasRecentlyRendered(0.5)) {
+			continue; // skip the creature if it is not rendered
+		}
+		float health;
+		bool broken;
+		int level;
+		IAI_CreatureInteractableInterface* interactableInterface = Cast<IAI_CreatureInteractableInterface>(interactable);
+		interactableInterface->Execute_GetInteractableStats(interactable, health, broken, level);
+
+		float currentValue = 0;
+
+		
+		currentValue = (health * interactableHealthValueMultiplier);
+		if (broken) { 
+			currentValue *= interactableIsBrokenMultiplier; 
+			result.brokenStructureVisible = true;
+		}
+		currentValue += level * interactableLevelMultiplier;
+		if (level == 1) {
+			result.lowLevelStructureVisible = true;
+		}
+		UE_LOG(LogTemp, Warning, TEXT("interactable %s evaluated with : %f"), *interactable->GetName(), currentValue);
+		interactableMedian += currentValue;
+		c++;
 	}
 
-	statsMedian /= allCreatures.Num();
-	statsMedian *= statsMedianMultiplier;
 
-
-
-
-
+	result.value = statsMedian + interactableMedian;
 
 	// Generate a filename based on the current date
 	const FDateTime Now = FDateTime::Now();
--- Source/monstersSanctuary/PhotoManager.h
+++ Source/monstersSanctuary/PhotoManager.h
@@ -16,6 +16,25 @@ struct FEvaluateSceneResult {
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float value;
+
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="EvaluateHints")
+	bool brokenStructureVisible;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="EvaluateHints")
+	bool lowLevelStructureVisible;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="EvaluateHints")
+	bool CreatureIsntDoingAnything;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="EvaluateHints")
+	bool sadCreatureVisible;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="EvaluateHints")
+	bool needBedCreatureVisible;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="EvaluateHints")
+	bool needWaterCreatureVisible;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="EvaluateHints")
+	bool needFoodCreatureVisible;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="EvaluateHints")
+	bool needCleanCreatureVisible;
 };
 
 /**
@@ -27,19 +46,34 @@ class MONSTERSSANCTUARY_API UPhotoManager : public UObject
 	GENERATED_BODY()
 	
 public:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	
+	//creature evaluation
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Creature evaluation")
 	FCreatureStats statsEvaluateMultiplier;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Creature evaluation")
 	FCreatureStats statsEvaluateAdder;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Creature evaluation")
 	float statsMedianMultiplier;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Creature evaluation")
+	FCreatureStats statsMinimumToConsiderPositive;
+
+	// interactable evaluation
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interactable evaluation")
+	float interactableHealthValueMultiplier;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interactable evaluation")
+	float interactableIsBrokenMultiplier;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interactable evaluation")
+	float interactableLevelMultiplier;
+
 	
 
 	UFUNCTION(BlueprintCallable)
 	void EvaluateScene(
 		////inputs
 		TArray<AActor*> allCreatures,
-		//TArray<AActor*> allInteractable,
+		TArray<AActor*> allInteractable,
 		//TArray<AActor*> allFornitures,
 		// outputs
 		FEvaluateSceneResult& result
